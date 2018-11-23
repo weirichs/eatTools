02febb9a820bcacd298611e7c02394f0b7c7051a
Benjamin Becker <benj.becker@web.de>
2018-11-23 15:58:58 +0100

    0.1.3
    asNumericIfPossible updated to S3, old version removed; tests and documentation adapted

--- R/asNumericIfPossible.R
+++ R/asNumericIfPossible.R
@@ -1,21 +1,21 @@
 
-asNumeric_new <- function(x, maintain.factor.scores = TRUE, force.string = TRUE, transform.factors = FALSE) {
-  UseMethod("asNumeric_new")
+asNumericIfPossible <- function(x, maintain.factor.scores = TRUE, force.string = TRUE, transform.factors = TRUE) {
+  UseMethod("asNumericIfPossible")
 }
 
-asNumeric_new.default <- function(x, ...) {
+asNumericIfPossible.default <- function(x, ...) {
   stop("Unknown input type or class. Input has to be of type numeric, factor, character or data.frame.")
 }
 
-asNumeric_new.numeric <- function(x, ...) {
+asNumericIfPossible.numeric <- function(x, ...) {
   x
 }
 
-asNumeric_new.factor <- function(x, maintain.factor.scores = TRUE, force.string = TRUE, transform.factors = TRUE) {
+asNumericIfPossible.factor <- function(x, maintain.factor.scores = TRUE, force.string = TRUE, transform.factors = TRUE) {
   if(transform.factors == FALSE) return(x)
   if(maintain.factor.scores) {
     x <- as.character(x)
-    y <- asNumeric_new(x, maintain.factor.scores = maintain.factor.scores,
+    y <- asNumericIfPossible(x, maintain.factor.scores = maintain.factor.scores,
                        force.string = force.string)
     # Maintaint type factor if asNumeric was unsuccessful
     if(is.character(y)) return(as.factor(y))
@@ -24,7 +24,7 @@ asNumeric_new.factor <- function(x, maintain.factor.scores = TRUE, force.string 
   as.numeric(x)
 }
 
-asNumeric_new.character <- function(x, maintain.factor.scores = TRUE, force.string = TRUE, ...) {
+asNumericIfPossible.character <- function(x, force.string = TRUE, ...) {
   y <- tryCatch(as.numeric(x), warning = function(w) {
     out <- suppressWarnings(as.numeric(x))
     class(out) <- c("warning", "numeric")
@@ -44,20 +44,19 @@ asNumeric_new.character <- function(x, maintain.factor.scores = TRUE, force.stri
   y
 }
 
-#### tbd: anpassen, dass maintain.factor.scores keine Auswirkungen auf character variablen hat!!
-asNumeric_new.data.frame <- function(x, maintain.factor.scores = TRUE, force.string = TRUE, transform.factors = FALSE){
-  df_list <- Map(changeWarning_asNumeric_new, x = x, varName = names(x),
+asNumericIfPossible.data.frame <- function(x, maintain.factor.scores = TRUE, force.string = TRUE, transform.factors = TRUE){
+  df_list <- Map(changeWarning_asNumeric, x = x, varName = names(x),
                  maintain.factor.scores = maintain.factor.scores, force.string = force.string, transform.factors = transform.factors)
-  out <- as.data.frame(df_list)
-
+  out <- as.data.frame(df_list, stringsAsFactors = FALSE)
   row.names(out) <- attr(x, "row.names")
   out
 }
 
-changeWarning_asNumeric_new <- function(x, varName, maintain.factor.scores, force.string, transform.factors) {
-  y <- tryCatch(asNumeric_new(x, maintain.factor.scores = maintain.factor.scores, force.string = force.string, transform.factors = transform.factors),
+changeWarning_asNumeric <- function(x, varName, maintain.factor.scores, force.string, transform.factors) {
+  y <- tryCatch(asNumericIfPossible(x, maintain.factor.scores = maintain.factor.scores, force.string = force.string, transform.factors = transform.factors),
                 warning = function(w) {
-      out <- suppressWarnings(asNumeric_new(x, maintain.factor.scores = maintain.factor.scores, force.string = force.string, transform.factors = transform.factors))
+      out <- suppressWarnings(asNumericIfPossible(x, maintain.factor.scores = maintain.factor.scores,
+                                                  force.string = force.string, transform.factors = transform.factors))
       w <- sub("Variable", varName, w)
       warning(w, call. = FALSE)
       out
