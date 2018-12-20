8d5a54df73c94c3af3a88aba8c3a1caa87715e8d
weirichs <43958363+weirichs@users.noreply.github.com>
2018-12-20 23:12:41 +0100

    Update remove_funs.R

--- R/remove_funs.R
+++ R/remove_funs.R
@@ -30,11 +30,17 @@ halveString <- function (string, pattern, first = TRUE )  {
   allSplit<- strsplit(x = string, split = pattern)
   if(first==TRUE)  {
     ret <- as.matrix(data.frame(X1 = unlist(lapply(allSplit, FUN = function (l) { l[1]})),
-                                X2 = unlist(lapply(allSplit, FUN = function (l) {
-                                  if(length(l) == 1 )  { ret <- NA } else { ret <- paste(l[-1], collapse=pattern)}
+                                X2 = unlist(lapply(1:length(allSplit), FUN = function (l) {
+                                  if(length(allSplit[[l]]) == 1 )  { ret <- NA } else { ret <- substring(string[l], first = nchar(allSplit[[l]][1])+2 ) }
                                   return(ret)})), stringsAsFactors = FALSE))
   }  else  {
-    ret <- as.matrix(data.frame(X1 = unlist(lapply(allSplit, FUN = function (l) {paste(l[1:(length(l)-1)], collapse=pattern)})),
+    ret <- as.matrix(data.frame(X1 = unlist(lapply(1:length(allSplit), FUN = function (l) {
+                                     if (length(allSplit[[l]]) > 1 ) {
+                                         val <- substr(string[l],1,nchar(string[l])-(nchar(allSplit[[l]][length(allSplit[[l]])])+1))
+                                     }  else  {
+                                         val <- string[l]
+                                     }
+                                     return(val)})),
                                 X2 = unlist(lapply(allSplit, FUN = function (l) {
                                   if(length(l)==1) { ret <- NA}  else { ret <- l[length(l)]}
                                   return(ret)})), stringsAsFactors = FALSE))
