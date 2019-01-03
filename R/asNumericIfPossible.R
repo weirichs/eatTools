6dd4672241996ebe0bce1dfe9d59ee010a9cd90b
weirichs <43958363+weirichs@users.noreply.github.com>
2019-01-03 13:19:06 +0100

    0.1.9

--- R/asNumericIfPossible.R
+++ R/asNumericIfPossible.R
@@ -1,5 +1,4 @@
-
-asNumericIfPossible <- function(x, maintain.factor.scores = TRUE, force.string = TRUE, transform.factors = TRUE) {
+asNumericIfPossible <- function(x, maintain.factor.scores = TRUE, force.string = TRUE, transform.factors = TRUE, varName = NULL) {
   UseMethod("asNumericIfPossible")
 }
 
@@ -15,12 +14,12 @@ asNumericIfPossible.logical <- function(x, ...) {
   as.numeric(x)
 }
 
-asNumericIfPossible.factor <- function(x, maintain.factor.scores = TRUE, force.string = TRUE, transform.factors = TRUE) {
+asNumericIfPossible.factor <- function(x, maintain.factor.scores = TRUE, force.string = TRUE, transform.factors = TRUE, varName = NULL) {
   if(transform.factors == FALSE) return(x)
   if(maintain.factor.scores) {
     x <- as.character(x)
     y <- asNumericIfPossible(x, maintain.factor.scores = maintain.factor.scores,
-                       force.string = force.string)
+                       force.string = force.string, varName=varName)
     # Maintaint type factor if asNumeric was unsuccessful
     if(is.character(y)) return(as.factor(y))
     return(y)
@@ -28,7 +27,7 @@ asNumericIfPossible.factor <- function(x, maintain.factor.scores = TRUE, force.s
   as.numeric(x)
 }
 
-asNumericIfPossible.character <- function(x, force.string = TRUE, ...) {
+asNumericIfPossible.character <- function(x, force.string = TRUE, varName = NULL, ...) {
   y <- tryCatch(as.numeric(x), warning = function(w) {
     out <- suppressWarnings(as.numeric(x))
     class(out) <- c("warning", "numeric")
@@ -36,50 +35,51 @@ asNumericIfPossible.character <- function(x, force.string = TRUE, ...) {
   })
   if(force.string) {
     if("warning" %in% class(y)) {
-      warning("Variable has been coerced to numeric, NAs have been induced.", call. = FALSE)
+      warning(sub("Variable", paste0("'", varName,"'"), "Variable has been coerced to numeric, NAs have been induced."), call. = FALSE)
       class(y) <- class(y)[[2]]
     }
     return(y)
   }
   if("warning" %in% class(y)) {
-    warning("Variable can not be transformed to numeric. Use force.string = T to force this.", call. = FALSE)
+    warning(sub("Variable", paste0("'", varName,"'"), "Variable can not be transformed to numeric. Use force.string = TRUE to force this."), call. = FALSE)
     return(x)
   }
   y
 }
 
 asNumericIfPossible.data.frame <- function(x, maintain.factor.scores = TRUE, force.string = TRUE, transform.factors = TRUE){
-  df_list <- Map(changeWarning_asNumeric, x = x, varName = names(x),
-                 maintain.factor.scores = maintain.factor.scores, force.string = force.string, transform.factors = transform.factors)
+  df_list <- lapply( colnames(x), FUN = function(nam) { asNumericIfPossible(x=x[[nam]],maintain.factor.scores = maintain.factor.scores, force.string = force.string, transform.factors = transform.factors, varName = nam)})
   out <- as.data.frame(df_list, stringsAsFactors = FALSE)
+  colnames(out) <- colnames(x)
   row.names(out) <- attr(x, "row.names")
   out
 }
 
-changeWarning_asNumeric <- function(x, varName, maintain.factor.scores, force.string, transform.factors) {
-  y <- tryCatch(asNumericIfPossible(x, maintain.factor.scores = maintain.factor.scores, force.string = force.string, transform.factors = transform.factors),
-                toWarn = function(w) {
-      out <- suppressWarnings(asNumericIfPossible(x, maintain.factor.scores = maintain.factor.scores,
-                                                  force.string = force.string, transform.factors = transform.factors))
-      w <- sub("Variable", varName, w)
-      warning(w, call. = FALSE)
-      out
-  })
-  y
-}
+
+#changeWarning_asNumeric <- function(x, varName, maintain.factor.scores, force.string, transform.factors) {
+#  y <- tryCatch(asNumericIfPossible(x, maintain.factor.scores = maintain.factor.scores, force.string = force.string, transform.factors = transform.factors),
+#                toWarn = function(w) {
+#      out <- suppressWarnings(asNumericIfPossible(x, maintain.factor.scores = maintain.factor.scores,
+#                                                  force.string = force.string, transform.factors = transform.factors))
+#      w <- sub("Variable", varName, w)
+#      warning(w, call. = FALSE)
+#      out
+#  })
+#  y
+#}
 
 
 
 
 #### Archiv
-extract_original_call <- function(fun_name, escape) {
-  call_list <- lapply(sys.calls(), as.character)
-  call_vec <- unlist(lapply(call_list, function(l) l[[1]]))
-  call_number <- match(fun_name, call_vec)
-  if(is.na(call_number)) {
-    warning("Original function call could not be retrieved. ", escape, " has been inserted.")
-    return(as.list(rep(escape, 20)))
-  }
-  #browser()
-  as.list(sys.calls()[[call_number]])
-}
+#extract_original_call <- function(fun_name, escape) {
+#  call_list <- lapply(sys.calls(), as.character)
+#  call_vec <- unlist(lapply(call_list, function(l) l[[1]]))
+#  call_number <- match(fun_name, call_vec)
+#  if(is.na(call_number)) {
+#    warning("Original function call could not be retrieved. ", escape, " has been inserted.")
+#    return(as.list(rep(escape, 20)))
+#  }
+#  #browser()
+#  as.list(sys.calls()[[call_number]])
+#}
