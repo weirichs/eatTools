3517385827c43d163968bf5640f85c3683b99f95
weirichs <43958363+weirichs@users.noreply.github.com>
2019-03-31 13:44:37 +0200

    0.1.15, bugfix convertLabel()

--- R/tools_aus_eatModel.r
+++ R/tools_aus_eatModel.r
@@ -30,19 +30,21 @@ createSpssSyntaxSnippet <- function ( dat , file, keep = TRUE ) {
 convertLabel <- function ( spssList , stringsAsFactors = TRUE, useZkdConvention = TRUE, replaceSpecialSigns = TRUE) {
                 if ( "data.frame" %in% class(spssList )) { stop ( "'spssList' must not be of class 'data.frame'.\n")}
                 varLabs<- attr(spssList, "variable.labels")
-                valLabs<- lapply ( spssList, attr, "value.labels")
+                class(valLabs)<- lapply ( spssList, attr, "value.labels")
                 if ( replaceSpecialSigns == TRUE ) {
-                     varLabs <- gsubAll ( string = varLabs, old = c("ä", "ü", "ö", "ß", "Ü"), new = c("ae", "ue", "oe", "ss", "Ue"))
+                     if(!is.null(varLabs)) {
+                         varLabs <- gsubAll ( string = varLabs, old = c("Ã¤", "Ã¼", "Ã¶", "ÃŸ", "Ãœ"), new = c("ae", "ue", "oe", "ss", "Ue"))
+                     }
                      valLabs <- lapply ( valLabs, FUN = function ( w ) {
                                 if ( !is.null(w)) {
-                                     names(w) <- gsubAll ( string = names(w), old = c("ä", "ü", "ö", "ß", "Ü"), new = c("ae", "ue", "oe", "ss", "Ue"))
+                                     names(w) <- gsubAll ( string = names(w), old = c("Ã¤", "Ã¼", "Ã¶", "ÃŸ", "Ãœ"), new = c("ae", "ue", "oe", "ss", "Ue"))
                                 }
                                 return(w) } )
                 }
                 if ( useZkdConvention == TRUE ) { zielnam <- c("varLabel", "valLabel") }  else  { zielnam <- c("variable.labels", "value.labels") }
                 datFr  <- data.frame ( spssList, stringsAsFactors = stringsAsFactors)
                 for ( u in 1:ncol(datFr)) {
-                      if ( !is.na(varLabs[colnames(datFr)[u]] )) { attr(datFr[,u], zielnam[1]) <- varLabs[[u]]}
+                      if ( !is.null(varLabs[colnames(datFr)[u]]) && !is.na(varLabs[colnames(datFr)[u]] )) { attr(datFr[,u], zielnam[1]) <- varLabs[[u]]}
                       if ( !is.null(valLabs[[colnames(datFr)[u]]] )) { attr(datFr[,u], zielnam[2]) <- valLabs[[u]]}
                 }
                 return(datFr)}
