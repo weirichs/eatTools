8636246e90cc7fa8e4f2bd456d7b6dfe8c086a7b
weirichs <43958363+weirichs@users.noreply.github.com>
2020-01-03 17:50:21 +0100

    0.2.2

--- R/desk.r
+++ R/desk.r
@@ -15,19 +15,25 @@ desk <- function(variable,na=NA, p.weights = NULL, na.rm = FALSE) {
             return(NULL)
          }
          ret      <- do.call("rbind", lapply(variable, FUN = function ( y ) {
-                     if(Mis.weight == TRUE ) {
+                     if(isTRUE(Mis.weight)) {
                         Summe      <- sum(y, na.rm = na.rm)
                         Mittelwert <- mean(y, na.rm = na.rm)
                         Varianz    <- var(y, na.rm = na.rm)
                         N          <- length(y)
-                        N.valid    <- length(na.omit(y)) }
-                     if(Mis.weight == FALSE ) {
-                        Summe      <- sum( y * p.weights )
+                        N.valid    <- length(na.omit(y))
+                        err        <- sd(y, na.rm = na.rm) / sqrt(length(na.omit(y)))
+                     }
+                     if(isFALSE(Mis.weight)) {
+                        Summe      <- sum( y * p.weights , na.rm = na.rm)
                         Mittelwert <- wtd.mean(x = y, weights = p.weights, na.rm = na.rm)
                         Varianz    <- wtd.var(x = y, weights = p.weights, na.rm = na.rm)
                         N          <- sum(p.weights)
-                        N.valid    <- sum(p.weights[which(!is.na(y))]) }
-                     dataFrame <- data.frame ( N = N, N.valid = N.valid, Missing = length(y) - length(na.omit(y)), Minimum = min(y, na.rm = na.rm), Maximum = max(y, na.rm = na.rm), Summe = Summe, Mittelwert = Mittelwert, std.err = sd(y, na.rm = na.rm) / sqrt(length(na.omit(y))), sig = ifelse(length(table(y))==1, NA, t.test(x = y)$p.value), Median = median(y, na.rm = na.rm), Streuung = sqrt(Varianz), Varianz = Varianz , stringsAsFactors = FALSE )
+                        N.valid    <- sum(p.weights[which(!is.na(y))])          ### 't1' = erster Term der Formel
+                        t1         <- length(na.omit(y)) / ( (length(na.omit(y)) - 1) * sum(p.weights)^2)
+                        t2         <- sum(p.weights^2 * ((na.omit(y) - Mittelwert)^2))
+                        err        <- sqrt(t1*t2)                               ### https://en.wikipedia.org/wiki/Weighted_arithmetic_mean
+                     }                                                          ### Abschnitt 'Bootstrapping validation'
+                     dataFrame <- data.frame ( N = N, N.valid = N.valid, Missing = length(y) - length(na.omit(y)), Minimum = min(y, na.rm = na.rm), Maximum = max(y, na.rm = na.rm), Summe = Summe, Mittelwert = Mittelwert, std.err = err, sig = ifelse(length(table(y))==1, NA, t.test(x = y)$p.value), Median = median(y, na.rm = na.rm), Streuung = sqrt(Varianz), Varianz = Varianz , stringsAsFactors = FALSE )
                      return(dataFrame)} ))
          rownames(ret) <- colnames(variable)
          return(ret)}
