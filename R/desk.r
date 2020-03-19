189832d8784d129dd1d92c76e04cf2909697e793
weirichs <43958363+weirichs@users.noreply.github.com>
2020-03-19 15:08:10 +0100

    0.2.5

--- R/desk.r
+++ R/desk.r
@@ -16,24 +16,24 @@ desk <- function(variable,na=NA, p.weights = NULL, na.rm = FALSE) {
          }
          ret      <- do.call("rbind", lapply(variable, FUN = function ( y ) {
                      if(isTRUE(Mis.weight)) {
-                        Summe      <- sum(y, na.rm = na.rm)
-                        Mittelwert <- mean(y, na.rm = na.rm)
-                        Varianz    <- var(y, na.rm = na.rm)
-                        N          <- length(y)
-                        N.valid    <- length(na.omit(y))
-                        err        <- sd(y, na.rm = na.rm) / sqrt(length(na.omit(y)))
+                        Sum    <- sum(y, na.rm = na.rm)
+                        Mean   <- mean(y, na.rm = na.rm)
+                        Var    <- var(y, na.rm = na.rm)
+                        N      <- length(y)
+                        N.valid<- length(na.omit(y))
+                        err    <- sd(y, na.rm = na.rm) / sqrt(length(na.omit(y)))
                      }
                      if(isFALSE(Mis.weight)) {
-                        Summe      <- sum( y * p.weights , na.rm = na.rm)
-                        Mittelwert <- wtd.mean(x = y, weights = p.weights, na.rm = na.rm)
-                        Varianz    <- wtd.var(x = y, weights = p.weights, na.rm = na.rm)
-                        N          <- sum(p.weights)
-                        N.valid    <- sum(p.weights[which(!is.na(y))])          ### 't1' = erster Term der Formel
-                        t1         <- length(na.omit(y)) / ( (length(na.omit(y)) - 1) * sum(p.weights)^2)
-                        t2         <- sum(p.weights^2 * ((na.omit(y) - Mittelwert)^2))
-                        err        <- sqrt(t1*t2)                               ### https://en.wikipedia.org/wiki/Weighted_arithmetic_mean
+                        Sum    <- sum( y * p.weights , na.rm = na.rm)
+                        Mean   <- wtd.mean(x = y, weights = p.weights, na.rm = na.rm)
+                        Var    <- wtd.var(x = y, weights = p.weights, na.rm = na.rm)
+                        N      <- sum(p.weights)
+                        N.valid<- sum(p.weights[which(!is.na(y))])              ### 't1' = erster Term der Formel
+                        t1     <- length(na.omit(y)) / ( (length(na.omit(y)) - 1) * sum(p.weights)^2)
+                        t2     <- sum(p.weights^2 * ((na.omit(y) - Mean)^2))
+                        err    <- sqrt(t1*t2)                                   ### https://en.wikipedia.org/wiki/Weighted_arithmetic_mean
                      }                                                          ### Abschnitt 'Bootstrapping validation'
-                     dataFrame <- data.frame ( N = N, N.valid = N.valid, Missing = length(y) - length(na.omit(y)), Minimum = min(y, na.rm = na.rm), Maximum = max(y, na.rm = na.rm), Summe = Summe, Mittelwert = Mittelwert, std.err = err, sig = ifelse(length(table(y))==1, NA, t.test(x = y)$p.value), Median = median(y, na.rm = na.rm), Streuung = sqrt(Varianz), Varianz = Varianz , stringsAsFactors = FALSE )
+                     dataFrame <- data.frame ( N = N, N.valid = N.valid, Missing = length(y) - length(na.omit(y)), Minimum = min(y, na.rm = na.rm), Maximum = max(y, na.rm = na.rm), Sum = Sum, Mean = Mean, std.err = err, sig = ifelse(length(table(y))==1, NA, t.test(x = y)$p.value), Median = median(y, na.rm = na.rm), SD = sqrt(Var), Var = Var , stringsAsFactors = FALSE )
                      return(dataFrame)} ))
          rownames(ret) <- colnames(variable)
          return(ret)}
