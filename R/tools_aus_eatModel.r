3ab32c74fc1631fa286d3006537c78b9c8b197ab
weirichs <43958363+weirichs@users.noreply.github.com>
2020-03-25 21:40:12 +0100

    0.2.5

--- R/tools_aus_eatModel.r
+++ R/tools_aus_eatModel.r
@@ -194,3 +194,23 @@ aggregateDataOld <- function(all.daten,spalten, unexpected.pattern.as.na = TRUE,
         if(length(noAgg) > 0) {dat.sum <- data.frame(all.daten[,noAgg, drop=FALSE],dat.sum,stringsAsFactors = FALSE)
         dat.agg <- data.frame(all.daten[,noAgg, drop=FALSE],dat.agg,stringsAsFactors = FALSE)}
         return(list(sum=dat.sum, agg=dat.agg, pc.list=list.pc))}
+
+whereAre <- function(a,b,quiet=FALSE) {
+            b <- data.frame(1:length(b),b,stringsAsFactors=FALSE)               ### zusaetzliche Syntaxbefehle sind notwendig, damit die Funktion mit missing values umgehen kann.
+            if(sum(which(is.na(a)))>0)     {cat("a contains missing values. \n")}
+            if(sum(which(is.na(b[,2])))>0) {cat("b contains missing values. \n")}
+            if(length(na.omit(a)) > length(unique(na.omit(a))))     {cat("a contains duplicate elements. \n")}
+            if(length(intersect(a,b[,2])) == 0) {
+               cat("No common elements in a and b. \n")
+               reihe <- NULL
+            }  else {
+               if(quiet==FALSE) { if(length(intersect(a,b[,2])) > 0) {if(length(setdiff(a,b[,2]))>0)      {cat("Not all Elemente of a included in b. \n")} } }
+               a <- na.omit(unique(a))                                          ### Sofern vorhanden, werden missing values aus a entfernt
+               b <- na.omit(b)                                                  ### Sofern vorhanden, werden missing values aus b entfernt; aber: Rangplatz der
+               if(length(a)>0) {                                                ### der nicht fehlenden Elemente in b bleibt erhalten
+                  reihe <- b[ which(b[,2] %in% a)  ,1]
+                  if(quiet==FALSE) { cat(paste("Found",length(reihe),"elements.\n")) }
+               }
+               if(length(a)==0) {cat("No valid values in a.\n")}
+            }
+            return(reihe)}
