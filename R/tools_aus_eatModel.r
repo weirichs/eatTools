749c5109f66398835a0c3aab472199c9b0cd267f
Sebastian Weirich <sebastian.weirich@iqb.hu-berlin.de>
2020-04-21 15:41:01 +0200

    0.2.11
    remove functions from eatTools to eatPrep

--- R/tools_aus_eatModel.r
+++ R/tools_aus_eatModel.r
@@ -156,44 +156,6 @@ tablePattern <- function(x, pattern = NULL, weights, na.rm = TRUE, useNA = c("no
                   }
                 return(Table)}
 
-aggregateDataOld <- function(all.daten,spalten, unexpected.pattern.as.na = TRUE, verboseAll = FALSE ) {
-        if(missing(spalten)) {spalten <- colnames(all.daten)}
-        spalten <- existsBackgroundVariables(dat = all.daten, variable=spalten)
-        noAgg <- setdiff(colnames(all.daten), spalten)
-        daten <- all.daten[,spalten, drop=FALSE]
-        foo   <- table(nchar(colnames(daten)))                                  ### Haben alle Variablennamen die gleiche Anzahl Zeichen?
-        if(length(foo)>1) {cat("Variable names with mixed numbers of characters.\n")}
-        items     <- unique(substr(colnames(daten),1,nchar(colnames(daten))-1)) ### wieviele Items wird es geben?
-        cat(paste("Aggregate ",ncol(daten)," variable(s) to ",length(items)," item(s).\n",sep="")); flush.console()
-        dat.sum <- NULL; dat.agg <- NULL; list.pc <- NULL                       ### erstelle leere Datenobjekte fuer Summendatensatz, aggregierten Datensatz und Liste mit partial-credit-Items
-        for (i in 1:length(items))      {
-          sub.dat      <- data.frame ( lapply( data.frame(daten[, which(substr(colnames(daten),1,nchar(colnames(daten))-1) %in% items[i]), drop=FALSE ], stringsAsFactors = FALSE), as.numeric), stringsAsFactors = FALSE)
-          ncol.sub.dat <- ncol(sub.dat)
-          last.sign    <- names(table(substr(colnames(sub.dat),nchar(colnames(sub.dat)),nchar(colnames(sub.dat)))))
-          toCheck      <- sum((last.sign)==letters[1:length(last.sign)])==length(last.sign)
-    ### Check: Ist das letzte Zeichen des Variablennamens immer ein Buchstabe und aufsteigend?
-          if(!toCheck) { cat(paste("Item ",items[i],": last character of variable names does not correspond to assumed alphabetic sequence.\n", sep="")); flush.console()}
-          isNA         <- table(rowSums(is.na(sub.dat)))
-          isNA.names   <- as.numeric(names(isNA))
-          unexpected   <- setdiff(isNA.names, c(0,ncol.sub.dat))
-          if( length( unexpected ) > 0  )   {
-            cases      <- sum(as.numeric(isNA[as.character(unexpected)]))
-            cat(paste("Caution! Found unexpected missing pattern in variables for item ",items[i], " in ",cases," cases.\n", sep= "" ) ) ; flush.console()
-            whichUnexp <- which( rowSums(is.na(sub.dat)) %in% unexpected)
-            if (verboseAll == TRUE) {cat("   Cases in question: "); cat(paste(whichUnexp, collapse=", ")); cat("\n")}
-          }
-          if(ncol.sub.dat == 1) {sub.dat[,"summe"] <- sub.dat[,1]}
-          if(ncol.sub.dat >  1) {sub.dat[,"summe"] <- apply(sub.dat, 1, FUN = function ( uu ) {ifelse( all(is.na(uu)), NA, sum(uu, na.rm=!unexpected.pattern.as.na))}) }
-          sub.dat[,"aggregiert"] <- ifelse(sub.dat$summe == ncol.sub.dat,1,0)
-          if(is.null(dat.sum)) { dat.sum <- sub.dat[,"summe", drop=FALSE] }     else { dat.sum <- cbind(dat.sum,sub.dat[,"summe", drop=FALSE]) }
-          if(is.null(dat.agg)) { dat.agg <- sub.dat[,"aggregiert",drop=FALSE] } else { dat.agg <- cbind(dat.agg,sub.dat[,"aggregiert",drop=FALSE]) }
-          colnames(dat.sum)[i] <- items[i]
-          colnames(dat.agg)[i] <- items[i]
-          maximum <- max(dat.sum[,i],na.rm = TRUE)                              ### ist das i-te Item partial credit?
-          if(maximum>1) {list.pc <- rbind(list.pc, data.frame(Var=items[i],pc=paste(names(table(dat.sum[,i])),collapse=", "),max=max(as.numeric(names(table(dat.sum[,i])))),stringsAsFactors = FALSE))}}
-        if(length(noAgg) > 0) {dat.sum <- data.frame(all.daten[,noAgg, drop=FALSE],dat.sum,stringsAsFactors = FALSE)
-        dat.agg <- data.frame(all.daten[,noAgg, drop=FALSE],dat.agg,stringsAsFactors = FALSE)}
-        return(list(sum=dat.sum, agg=dat.agg, pc.list=list.pc))}
 
 whereAre <- function(a,b,quiet=FALSE) {
             b <- data.frame(1:length(b),b,stringsAsFactors=FALSE)               ### zusaetzliche Syntaxbefehle sind notwendig, damit die Funktion mit missing values umgehen kann.
--- R/unzip.wa.r
+++
@@ -1,7 +0,0 @@
-unzip.wa <- function ( zipfile, targetdir = tempdir(), unzip.exe = "c:/Program Files/tools/unzip.exe") {
-     old_wd <- getwd()
-     on.exit(setwd(old_wd))
-     setwd(targetdir)
-     system(paste0("\"",normalizePath(unzip.exe),"\" \"",normalizePath(zipfile),"\""), show.output.on.console = TRUE)}
-
-
