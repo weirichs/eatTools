7f12e79772888a96e4c7d05c7d3a4b72bae02171
Sebastian Weirich <sebastian.weirich@iqb.hu-berlin.de>
2020-07-09 15:11:09 +0200

    0.2.25
    minor bugfixes in tableUnlist()

--- R/tableUnlist.r
+++ R/tableUnlist.r
@@ -1,9 +1,8 @@
 ### Hilfsfunktion, ersetzt table(unlist( ... ))
 tableUnlist <- function(dataFrame, verbose = TRUE, useNA = c("no","ifany", "always"))   {
                 useNA<- match.arg(useNA)
-                # if(class(dataFrame) != "data.frame" ) {stop("Argument of 'tableUnlist' has to be of class 'data.frame'.\n")}
-                if(class(dataFrame) != "data.frame" ) {
-                   if(verbose == TRUE ) {cat(paste("Warning! Argument of 'tableUnlist' has to be of class 'data.frame'. Object will be converted to data.frame.\n",sep=""))}
+                if( !"data.frame" %in% class(dataFrame) ) {
+                   if(verbose) {cat(paste("Warning! Argument of 'tableUnlist' has to be of class 'data.frame'. Object will be converted to data.frame.\n",sep=""))}
                    dataFrame <- data.frame(dataFrame, stringsAsFactors=FALSE)
                 }
                 vek  <- do.call("c", lapply(dataFrame, FUN = function ( col ) { return(col)}))
