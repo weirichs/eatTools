de96c9535f8bee55ec40c11ea940d48371d99d47
Sebastian Weirich <sebastian.weirich@iqb.hu-berlin.de>
2021-05-17 13:33:38 +0200

    bugfix descr()

--- R/descr.r
+++ R/descr.r
@@ -26,13 +26,15 @@ descr <- function(variable,na=NA, p.weights = NULL, na.rm = FALSE, verbose=TRUE)
                         err    <- sd(y, na.rm = na.rm) / sqrt(length(na.omit(y)))
                      }
                      if(isFALSE(Mis.weight)) {
+                        valid  <- which(!is.na(y))
+                        if ( length(which(is.na(p.weights)))>0) { warning("Found ",length(which(is.na(p.weights))), " missing values on weighting variable.")  }
                         Sum    <- sum( y * p.weights , na.rm = na.rm)
                         Mean   <- weighted.mean(x = y, w = p.weights, na.rm = na.rm)
                         Var    <- wtdVar(x = y, weights = p.weights, na.rm = na.rm)
                         N      <- sum(p.weights)
-                        N.valid<- sum(p.weights[which(!is.na(y))])              ### 't1' = erster Term der Formel
-                        t1     <- length(na.omit(y)) / ( (length(na.omit(y)) - 1) * sum(p.weights)^2)
-                        t2     <- sum(p.weights^2 * ((na.omit(y) - Mean)^2))
+                        N.valid<- sum(p.weights[valid])                         ### 't1' = erster Term der Formel
+                        t1     <- length(na.omit(y)) / ( (length(na.omit(y)) - 1) * sum(p.weights[valid])^2)
+                        t2     <- sum(p.weights[valid]^2 * ((na.omit(y) - Mean)^2))
                         err    <- sqrt(t1*t2)                                   ### https://en.wikipedia.org/wiki/Weighted_arithmetic_mean
                      }                                                          ### Abschnitt 'Bootstrapping validation'
                      dataFrame <- data.frame ( N = N, N.valid = N.valid, Missing = length(y) - length(na.omit(y)), Minimum = min(y, na.rm = na.rm), Maximum = max(y, na.rm = na.rm), Sum = Sum, Mean = Mean, std.err = err, sig = ifelse(length(table(y))==1, NA, t.test(x = y)$p.value), Median = median(y, na.rm = na.rm), SD = sqrt(Var), Var = Var , stringsAsFactors = FALSE )
