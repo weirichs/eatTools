daae7296b72033b858c2bab50f84547ff6e1ccfa
Benjamin Becker <benj.becker@web.de>
2021-10-13 14:49:04 +0200

    polish roundDF

--- R/roundDF.R
+++ R/roundDF.R
@@ -1,11 +1,15 @@
 
-roundDF <- function(df, roundN = 3) {
-  roundCol <- function(var, roundN) {
-    if(!is.numeric(var)) return(var)
-    round(var, digits = roundN)
-  }
+roundDF <- function(dat, digits = 3) {
+  if(!is.data.frame(dat)) stop("'dat' needs to be a data.frame.")
+  if(!is.numeric(digits) || length(digits) != 1) stop("'digits' needs to be a numeric vector of length 1.")
 
-  out <- as.data.frame(lapply(df, roundCol, roundN = roundN), stringsAsFactors = FALSE)
-  names(out) <- names(df)
+  out <- as.data.frame(lapply(dat, roundCol, digits = digits), stringsAsFactors = FALSE)
+  colnames(out) <- colnames(dat)
+  rownames(out) <- rownames(dat)
   out
+}
+
+roundCol <- function(var, digits) {
+  if(!is.numeric(var)) return(var)
+  round(var, digits = digits)
 }
--- tests/testthat/test_roundDF.R
+++ tests/testthat/test_roundDF.R
@@ -1,9 +1,17 @@
 
+test_that("roundDF errors", {
+  expect_error(roundDF(0), "'dat' needs to be a data.frame.")
+  expect_error(roundDF(mtcars, "a"), "'digits' needs to be a numeric vector of length 1.")
+  expect_error(roundDF(mtcars, 1:2), "'digits' needs to be a numeric vector of length 1.")
+})
+
+
 test_that("roundDF", {
   out <- roundDF(mtcars, 0)
   expect_equal(names(out), names(mtcars))
   expect_equal(out$disp, round(mtcars$disp, 0))
   expect_equal(dim(out), dim(mtcars))
+  expect_equal(rownames(out), rownames(mtcars))
 
   out2 <- roundDF(iris, 0)
   expect_equal(names(out2), names(iris))
