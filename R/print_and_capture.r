9ae1b326b080989d7b13bfa8926371f7364f434f
Benjamin Becker <benj.becker@web.de>
2022-06-28 11:04:49 +0200

    fix cmd notes

--- R/print_and_capture.r
+++ R/print_and_capture.r
@@ -1,5 +1,5 @@
 print_and_capture <- function(x, spaces = 0) {
- str1 <- capture.output(print(x))
+ str1 <- utils::capture.output(print(x))
  if ( spaces > 0) {
       str2 <- paste(rep(" ", spaces), collapse="")
       str1 <- paste(str2, str1)
