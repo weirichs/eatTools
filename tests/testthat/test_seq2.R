8fc950405ef12efb1aa567b7cbb657e45d07e39b
grewered <grewered@hu-berlin.de>
2024-01-19 12:01:45 +0100

    added test to test for type/whether checkmate throws the right error

--- tests/testthat/test_seq2.R
+++ tests/testthat/test_seq2.R
@@ -4,3 +4,19 @@ test_that("seq2", {
   expect_identical(seq2(-3, 0), -3:0)
   expect_identical(seq2(1, 0), integer())
 })
+test_that("non numeric input for seq2 throws an error", {
+  expect_error(
+    seq2("d", "t"),
+    "Must be of type 'number', not 'character'"
+  )
+  expect_error(
+    seq2("d"),
+    "Must be of type 'number', not 'character'"
+  )
+  expect_error(
+    seq2(TRUE, FALSE),
+    "Must be of type 'number', not 'logical'"
+  )
+})
+# add check for argument length
+
