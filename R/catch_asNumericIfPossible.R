b7605f6b03fbfd1d5e209b641b8156d6eb417b2f
grewered <grewered@hu-berlin.de>
2024-02-09 14:03:37 +0100

    added checks: catch_asNumericIfPossible

--- R/catch_asNumericIfPossible.R
+++ R/catch_asNumericIfPossible.R
@@ -1,6 +1,11 @@
 
 
 catch_asNumericIfPossible <- function(x, warn, maintain.factor.scores = TRUE, force.string = TRUE, transform.factors = TRUE) {
+  checkmate::assert_logical(maintain.factor.scores, len = 1)
+  checkmate::assert_logical(force.string, len = 1)
+  checkmate::assert_logical(transform.factors, len = 1)
+  checkmate::assert_character(warn, len = 1)
+
   y <- tryCatch(asNumericIfPossible(x, maintain.factor.scores = maintain.factor.scores, force.string = force.string,
                                     transform.factors = transform.factors),
                 warning = function(w) {
