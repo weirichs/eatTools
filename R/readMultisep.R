87fc5c15a92fed99b7ab0237535535e18ffac578
grewered <grewered@hu-berlin.de>
2024-03-14 13:45:10 +0100

    assertions for readMultisep

--- R/readMultisep.R
+++ R/readMultisep.R
@@ -1,8 +1,8 @@
 readMultisep <- function(file,sep) {
+  checkmate::assert_file(file)
+  checkmate::assert_character(sep)
   lines <- readLines(file)
   datf <- data.frame(do.call(rbind,strsplit(lines, sep, fixed = TRUE)), stringsAsFactors=FALSE)
   datf <- data.frame(lapply(datf,utils::type.convert,as.is=TRUE), stringsAsFactors=FALSE)
   return(datf)
 }
-
-
