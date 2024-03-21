f713eabbb8ef295527cc318bf82b43be580066ae
grewered <grewered@hu-berlin.de>
2024-03-21 14:27:06 +0100

    added len = 1

--- R/print_and_capture.r
+++ R/print_and_capture.r
@@ -1,6 +1,6 @@
 print_and_capture <- function(x, spaces = 0) {
   checkmate::assert_vector(x)
-  checkmate::assert_numeric(spaces, lower = 0)
+  checkmate::assert_numeric(spaces, lower = 0, len = 1)
  str1 <- utils::capture.output(print(x))
  if ( spaces > 0) {
       str2 <- paste(rep(" ", spaces), collapse="")
