55b70cf869037ec9cc5d2ba36f67902165cc77c8
grewered <grewered@hu-berlin.de>
2024-03-21 14:32:29 +0100

    added len = 1

--- R/readMultisep.R
+++ R/readMultisep.R
@@ -1,5 +1,5 @@
 readMultisep <- function(file,sep) {
-  checkmate::assert_character(sep)
+  checkmate::assert_character(sep, len = 1)
   lines <- readLines(file)
   datf <- data.frame(do.call(rbind,strsplit(lines, sep, fixed = TRUE)), stringsAsFactors=FALSE)
   datf <- data.frame(lapply(datf,utils::type.convert,as.is=TRUE), stringsAsFactors=FALSE)
