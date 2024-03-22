ea93711210473669ea1ff220fe49966e7f328f1b
sachseka <sachseka@hu-berlin.de>
2024-03-22 10:55:33 +0100

    enhance code coverage test_that for readMultiSep

--- R/readMultisep.R
+++ R/readMultisep.R
@@ -1,7 +1,15 @@
-readMultisep <- function(file,sep) {
+readMultisep <- function(file,sep,colnames=TRUE) {
   lines <- readLines(file)
-  datf <- data.frame(do.call(rbind,strsplit(lines, sep, fixed = TRUE)), stringsAsFactors=FALSE)
+  lines2 <- strsplit(lines, sep, fixed = TRUE)
+  lines2 <- lapply(lines2, function(hh) hh <- gsub("\"", "", hh))
+  if(colnames) {
+    datf <- data.frame(do.call(rbind,lines2[-1]), stringsAsFactors=FALSE)
+    names(datf) <- unlist(lines2[1])
+  } else {
+    datf <- data.frame(do.call(rbind,lines2), stringsAsFactors=FALSE)
+  }
   datf <- data.frame(lapply(datf,utils::type.convert,as.is=TRUE), stringsAsFactors=FALSE)
+  datf <- data.frame(lapply(datf, function(hh) if(is.character(hh)) {hh <- gsub("\"", "", hh)} else hh))
   return(datf)
 }
 
