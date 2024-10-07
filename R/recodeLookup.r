013898d97341631ebd25b90d610a681de5750966
Benjamin Becker <benj.becker@web.de>
2024-10-07 11:08:22 +0200

    Revert "bugfix in recodeLookup"
    This reverts commit c9d3b54f99c1eec20cf89a7b0d1b20fe03121982.

--- R/recodeLookup.r
+++ R/recodeLookup.r
@@ -1,7 +1,7 @@
 # var    ... variable
 # lookup ... data.frame mit zwei spalten; erste spalte: alte Werte; zweite Spalte: neue Werte
 recodeLookup <- function (var, lookup) {
-        if(is.null(lookup) || is.null(var)) {return(var)}
+        if(is.null(lookup)) {return(var)}
         lookup <- makeDataFrame(lookup)
         if(ncol(lookup) != 2) {stop("The lookup table must have two colums.")}     ### checks ...
         if(length(unique(lookup[,1])) < nrow(lookup) ) { stop("Old values in lookup table are not unique.")}
@@ -9,12 +9,11 @@ recodeLookup <- function (var, lookup) {
                 classes <- class(x)
                 if("haven_labelled" %in% classes) stop("Variable class 'haven_labelled' is not supported by recodeLookup.")
                 })
+
         not_in_lookup <- setdiff(unique(var), lookup[, 1])
-        if(length(not_in_lookup)>0) {
-            add_lookup <- data.frame(not_in_lookup, not_in_lookup, stringsAsFactors = FALSE)
-            names(add_lookup) <- names(lookup)
-            lookup <- rbind(lookup, add_lookup)
-        }
+        add_lookup <- data.frame(not_in_lookup, not_in_lookup, stringsAsFactors = FALSE)
+        names(add_lookup) <- names(lookup)
+        lookup <- rbind(lookup, add_lookup)
         return(lookup[match(var, lookup[, 1]), 2])
 }
 
