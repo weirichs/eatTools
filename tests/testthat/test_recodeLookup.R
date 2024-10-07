54c82f946cf6c7613cd99192c1a687634ec20918
Sebastian Weirich <weirichs@user.hu-berlin.de>
2024-10-07 15:01:22 +0200

    add tests for recodeLookup

--- tests/testthat/test_recodeLookup.R
+++ tests/testthat/test_recodeLookup.R
@@ -13,7 +13,6 @@ test_that("Recode numeric", {
   lookup1 <- data.frame(old = 1:3, new = 3:1)
   out <- recodeLookup(1:3, lookup1)
   expect_equal(out, 3:1)
-
   lookup1 <- data.frame(old = 1:3, new = 3:1)
   out <- recodeLookup(1:10, lookup1)
   expect_equal(out, c(3:1, 4:10))
@@ -29,7 +28,6 @@ test_that("Recode with factors", {
   lookup1 <- data.frame(old = letters[1:3], new = letters[5:3], stringsAsFactors = FALSE)
   out <- recodeLookup(factor(letters[1:3]), lookup1)
   expect_equal(out, letters[5:3])
-
   lookup2 <- data.frame(old = letters[1:3], new = letters[5:3], stringsAsFactors = TRUE)
   out2 <- recodeLookup(letters[1:3], lookup2)
   expect_equal(out2, factor(letters[5:3]))
@@ -39,11 +37,9 @@ test_that("Recode with NAs", {
   lookup1 <- data.frame(old = 1:3, new = 3:1)
   out <- recodeLookup(c(NA, 1, 5, 3, NA), lookup1)
   expect_equal(out, c(NA, 3, 5, 1, NA))
-
   lookup2 <- data.frame(old = c(NA, 1, 2), new = c(3, 2, 1))
   out2 <- recodeLookup(c(NA, 1, 5, 3, NA), lookup2)
   expect_equal(out2, c(3, 2, 5, 3, 3))
-
   lookup3 <- data.frame(old = c(NA, 1, 2), new = c(3, NA, 10))
   out3 <- recodeLookup(c(NA, 1, 5, 3, NA), lookup3)
   expect_equal(out3, c(3, NA, 5, 3, 3))
@@ -53,11 +49,9 @@ test_that("Recode into/from different types", {
   lookup1 <- data.frame(old = 1:3, new = letters[1:3], stringsAsFactors = FALSE)
   out <- recodeLookup(1:5, lookup1)
   expect_equal(out, c(letters[1:3],4:5))
-
   lookup2 <- data.frame(old = letters[1:3], new = 1:3, stringsAsFactors = FALSE)
   out2 <- recodeLookup(letters[1:5], lookup2)
   expect_equal(out2, c(1:3, letters[4:5]))
-
 })
 
 test_that("Recode in data.frame", {
@@ -80,3 +74,12 @@ test_that("Recode labeled", {
                "Variable class 'haven_labelled' is not supported by recodeLookup.")
 })
 
+
+test_that("recode with null", {
+  mtcars1 <- mtcars
+  lookup1 <- data.frame(old = c(4, 6, 8), new = c(40, 60, 80))
+  looktab <- NULL
+  cars2 <- NULL
+  expect_true(is.null(recodeLookup(mtcars1, looktab)))
+  expect_true(is.null(recodeLookup(cars2, lookup1)))
+})
