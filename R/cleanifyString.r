f008d104cc1ae110b1a8fa5df5995556b966e6c4
Edna Gwendolyn Daphne Grewers <grewered@user.hu-berlin.de>
2024-12-06 11:51:12 +0100

    added assertions. added visual more accessible structure

--- R/cleanifyString.r
+++ R/cleanifyString.r
@@ -1,8 +1,18 @@
-cleanifyString <- function(x, removeNonAlphaNum = TRUE, replaceSpecialChars = TRUE, oldEncoding = NULL, ...) {
+cleanifyString <- function(x, removeNonAlphaNum = TRUE, replaceSpecialChars = TRUE,
+                           oldEncoding = NULL, ...) {
+  lapply(c(removeNonAlphaNum, replaceSpecialChars, oldEncoding),
+         checkmate::assert_logical, len = 1)
   UseMethod("cleanifyString")
   }
 
-cleanifyString.character <- function(x, removeNonAlphaNum = TRUE, replaceSpecialChars = TRUE, oldEncoding = NULL, ...) {
+### ----------------------------------------------------------------------------
+
+cleanifyString.character <- function(x, removeNonAlphaNum = TRUE, replaceSpecialChars = TRUE,
+                                     oldEncoding = NULL, ...) {
+  lapply(c(removeNonAlphaNum, replaceSpecialChars, oldEncoding),
+         checkmate::assert_logical, len = 1)
+  checkmate::assert_character(x)
+
   if(!is.null(oldEncoding)) {
     x <- iconv(x, from=oldEncoding, to="UTF-8")
   }
@@ -15,22 +25,36 @@ cleanifyString.character <- function(x, removeNonAlphaNum = TRUE, replaceSpecial
 
   return(x)}
 
-cleanifyString.factor <- function(x, removeNonAlphaNum = TRUE, replaceSpecialChars = TRUE, oldEncoding = NULL, ...) {
+### ----------------------------------------------------------------------------
+
+cleanifyString.factor <- function(x, removeNonAlphaNum = TRUE, replaceSpecialChars = TRUE,
+                                  oldEncoding = NULL, ...) {
+  lapply(c(removeNonAlphaNum, replaceSpecialChars, oldEncoding),
+         checkmate::assert_logical, len = 1)
+
   old <- list(var = as.character(x), lev = levels(x))
   new <- lapply(old, cleanifyString,
-                removeNonAlphaNum = removeNonAlphaNum, replaceSpecialChars = replaceSpecialChars, oldEncoding = oldEncoding, ...)
+                removeNonAlphaNum = removeNonAlphaNum, replaceSpecialChars = replaceSpecialChars,
+                oldEncoding = oldEncoding, ...)
 
   #browser()
   varNew <- factor(x = new[[1]], levels = unique(new[[2]]))
 
   return(varNew)}
 
-cleanifyString.data.frame <- function(x, removeNonAlphaNum = TRUE, replaceSpecialChars = TRUE, oldEncoding = NULL, ...) {
+### ----------------------------------------------------------------------------
+
+cleanifyString.data.frame <- function(x, removeNonAlphaNum = TRUE, replaceSpecialChars = TRUE,
+                                      oldEncoding = NULL, ...) {
+  lapply(c(removeNonAlphaNum, replaceSpecialChars, oldEncoding),
+         checkmate::assert_logical, len = 1)
+  x <- makeDataFrame(x)
+
   for(i in 1:ncol(x)) {
     input_vec <- x[,i]
     if(is.character(input_vec) | is.factor(input_vec)) {
-      x[,i] <- cleanifyString(input_vec, removeNonAlphaNum = removeNonAlphaNum, replaceSpecialChars = replaceSpecialChars,
-                              oldEncoding = oldEncoding, ...)
+      x[,i] <- cleanifyString(input_vec, removeNonAlphaNum = removeNonAlphaNum,
+                              replaceSpecialChars = replaceSpecialChars, oldEncoding = oldEncoding, ...)
     }
   }
   return(x)}
