e3c5783e97d3f016bd970d405311e13269de2b90
Ben <benj.becker@web.de>
2024-12-06 15:04:39 +0100

    cleanifyString: fix locale specific factor level sorting in tests

--- tests/testthat/test_cleanifyString.R
+++ tests/testthat/test_cleanifyString.R
@@ -1,5 +1,5 @@
-
 messy_char <- c("(te-st.)", "te\U00DFt", "t\U00E4st", "t\U00C4st", "test\U2026")
+messy_fac <- factor(messy_char, levels = c("(te-st.)", "te\U00DFt", "t\U00E4st", "t\U00C4st", "test\U2026"))
 
 test_that("cleanifyString on character vector", {
   out <- cleanifyString(messy_char)
@@ -13,21 +13,21 @@ test_that("cleanifyString on character vector", {
 })
 
 test_that("cleanifyString on factor", {
-  out <- cleanifyString(as.factor(messy_char))
+  out <- cleanifyString(messy_fac)
   expect_equal(out, factor(c("test", "tesst", "taest", "tAest", "test"),
-                               levels = c("test", "taest", "tAest", "tesst")))
+                               levels = c("test", "tesst", "taest", "tAest")))
 })
 
 test_that("cleanifyString on factor", {
   input_df <- data.frame(v1 = messy_char,
-                         v2 = as.factor(messy_char),
+                         v2 = messy_fac,
                          v3 = 1:5,
                          v4 = rep(TRUE, 5))
 
   out <- cleanifyString(input_df)
   expect_identical(out$v1, c("test", "tesst", "taest", "tAest", "test"))
   expect_equal(out$v2, factor(c("test", "tesst", "taest", "tAest", "test"),
-                               levels = c("test", "taest", "tAest", "tesst")))
+                               levels = c("test", "tesst", "taest", "tAest")))
   expect_equal(out$v3, 1:5)
   expect_equal(out$v4, rep(TRUE, 5))
 })
