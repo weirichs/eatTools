31af1aa5e64737d3f59906d665c1cac0b0be9aff
Sebastian Weirich <sebastian.weirich@iqb.hu-berlin.de>
2025-06-18 14:50:52 +0200

    new function checkBackgroundVariables

---
+++ tests/testthat/test_checkBackgroundVariables.R
@@ -0,0 +1,35 @@
+context("check for checking function of background variable consistency")
+data(data.timss1)
+
+
+varGroups <- list(ID = "IDSTUD", wgt = c("TOTWGT","JKZONE"), covars = c("female", "books", "lang", "migant"), depVar = "ASMMAT")
+
+test_that("error occurs due to type", {
+  lapply(varGroups, FUN = function (v) {
+  if(v[[1]] == "female") {expect_error(existsBackgroundVariables (dat=data.timss1[[1]], variable=v))}
+  })})
+
+# correction
+varGroups <- list(ID = "IDSTUD", wgt = c("TOTWGT","JKZONE"), covars = c("female", "books", "lang", "migrant"), depVar = "ASMMAT")
+varList   <- lapply(varGroups, FUN = function (v) {
+    existsBackgroundVariables (dat=data.timss1[[1]], variable=v)
+})
+
+# however, only one weighting variable is allowed
+test_that("error occurs because obly one wgt variable is allowed", {
+   expect_error(checkBackgroundVariables (varList, len1 = c("ID", "wgt", "depVar"), overlap = NULL))
+})
+
+# only one of the covariates should be used for group differences
+# difference variable must be included in covars
+varGroups <- list(ID = "IDSTUD", wgt = "TOTWGT", covars = c("female", "books", "lang"),
+             differencesFor = "migrant", depVar = "ASMMAT")
+varList   <- lapply(varGroups, FUN = function (v) {
+    existsBackgroundVariables (dat=data.timss1[[1]], variable=v)
+})
+
+test_that("error occurs because some variable groups must have only one element", {
+  expect_error(checkBackgroundVariables (varList, len1 = c("ID", "wgt", "depVar"),overlap = list(v1 = list(vars = c("covars","differencesFor"), len = 1))))
+})
+
+
--- tests/testthat/test_mergeAttr.R
+++ tests/testthat/test_mergeAttr.R
@@ -10,34 +10,32 @@ df2 <- data.frame ( id = as.factor(c(2,2,4)), status = factor ( c("married", "ma
 
 
 test_that("maintain label", {
-  messages <- capture_messages(out <- mergeAttr(df1, df2, all = TRUE, onlyVarValLabs = FALSE))
+  messages <- capture.output(out <- mergeAttr(df1, df2, all = TRUE, onlyVarValLabs = FALSE))
 
   expect_equal(attr(out[,"happy"], "variable.label"), "happieness in the workplace")
-  expect_equal(length(messages), 4)
-  expect_equal(messages[1], "1 of 2 unit(s) of merging variable 'id' from data set 'y' not included in data set 'x'.\n")
-  expect_equal(messages[2], "2 of 3 unit(s) of merging variable 'id' from data set 'x' not included in data set 'y'.\n")
-  expect_equal(messages[3], "Merging levels are not unique in data set 'y'.\n")
-  expect_equal(messages[4], "   Merging variable pair 'id'<==>'id' has different classes: 'integer'<==>'factor'. Classes will be homogenized to 'character'.\n   Use 'homoClass = FALSE' to suppress this behavior.\n")
+  expect_equal(length(messages), 5)
+  expect_equal(messages[1], "1 of 2 unit(s) of merging variable 'id' from data set 'y' not included in data set 'x'.")
+  expect_equal(messages[2], "2 of 3 unit(s) of merging variable 'id' from data set 'x' not included in data set 'y'.")
+  expect_equal(messages[3], "Merging levels are not unique in data set 'y'.")
+  expect_equal(messages[4], "   Merging variable pair 'id'<==>'id' has different classes: 'integer'<==>'factor'. Classes will be homogenized to 'character'.")
+  expect_equal(messages[5], "   Use 'homoClass = FALSE' to suppress this behavior.")
 })
 
-
-
 test_that("adapt messages", {
-  messages <- capture_messages(out <- mergeAttr(df1, df2, all = TRUE, onlyVarValLabs = FALSE, unitName = "student",
+  messages <- capture.output(out <- mergeAttr(df1, df2, all = TRUE, onlyVarValLabs = FALSE, unitName = "student",
                    xName = "student questionnaire", yName = "school questionnaire",
                    verbose = c("match", "unique")))
-
   expect_equal(attr(out[,"happy"], "variable.label"), "happieness in the workplace")
   expect_equal(length(messages), 3)
-  expect_equal(messages[1], "1 of 2 student(s) of merging variable 'id' from data set 'school questionnaire' not included in data set 'student questionnaire'.\n")
-  expect_equal(messages[2], "2 of 3 student(s) of merging variable 'id' from data set 'student questionnaire' not included in data set 'school questionnaire'.\n")
-  expect_equal(messages[3], "Merging levels are not unique in data set 'school questionnaire'.\n")
+  expect_equal(messages[1], "1 of 2 student(s) of merging variable 'id' from data set 'school questionnaire' not included in data set 'student questionnaire'.")
+  expect_equal(messages[2], "2 of 3 student(s) of merging variable 'id' from data set 'student questionnaire' not included in data set 'school questionnaire'.")
+  expect_equal(messages[3], "Merging levels are not unique in data set 'school questionnaire'.")
 })
 
 
 test_that("messages2", {
   df2[,"happy"] <- c(FALSE, FALSE, TRUE)
-  messages <- capture_messages(out <- mergeAttr(df1, df2, all = TRUE,
+  messages <- capture.output(out <- mergeAttr(df1, df2, all = TRUE,
      setAttr=FALSE, by="id", verbose="common", suffixes = c("_first", "_second")))
-  expect_equal(messages, "Additional common variables (beyond the 'by'-variables) found: 'happy'. Add suffixes '_first', '_second' to these variables in the result data.frame.\n")
+  expect_equal(messages, "Additional common variables (beyond the 'by'-variables) found: 'happy'. Add suffixes '_first', '_second' to these variables in the result data.frame.")
 })
